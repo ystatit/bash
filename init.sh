f2186e22231717730d313df575305c62f3a932ad
ystatit <59353905+ystatit@users.noreply.github.com>
2020-08-31 15:01:44 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo timedatectl set-timezone "Asia/Taipei"
+sudo timedatectl set-timezone Asia/Taipei
 sudo sh -c "echo color koehler >> /etc/vimrc"
 echo "PS1='\[\e[01;36m\]\u\[\e[01;37m\]@\[\e[01;33m\]\H\[\e[01;37m\]:\[\e[01;32m\]\w\[\e[01;37m\]\\$\[\033[0;37m\] '" | sudo tee -a /etc/bashrc
 echo 'HISTTIMEFORMAT="%F %T "' | sudo tee -a /etc/bashrc
