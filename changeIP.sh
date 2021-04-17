a45ae72943fbfca52ad4b99db2027ad48e29241a
ystatit <59353905+ystatit@users.noreply.github.com>
2021-04-17 13:34:44 +0800

    Create changeIP.sh

---
+++ changeIP.sh
@@ -0,0 +1,24 @@
+# Stop Services
+systemctl stop kubelet docker
+
+# Backup Kubernetes and kubelet
+mv /etc/kubernetes /etc/kubernetes-backup
+mv /var/lib/kubelet /var/lib/kubelet-backup
+
+# Keep the certs we need
+mkdir -p /etc/kubernetes
+cp -r /etc/kubernetes-backup/pki /etc/kubernetes
+rm -rf /etc/kubernetes/pki/{apiserver.*,etcd/peer.*}
+
+# Start docker
+systemctl start docker
+
+# Get current public ip address
+IP=$(curl -s ifconfig.me)
+
+# Init cluster with new ip address
+kubeadm init --control-plane-endpoint $IP \
+--ignore-preflight-errors=DirAvailable--var-lib-etcd
+
+# Verify resutl
+# kubectl cluster-info
